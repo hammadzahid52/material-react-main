5f065deac8a6d946d69b075dde5f755dc371ce8f
hammadzahid52 <hamadzahid57@gmail.com>
2024-01-30 16:27:33 +0500

    Update Card for Dark Mode

--- src/sections/userdata/view/Card.jsx
+++ src/sections/userdata/view/Card.jsx
@@ -11,13 +11,13 @@ Card.propTypes = {
 
 function Card({ first_name, last_name, username, email, avatar }) {
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center">
+    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white  flex flex-col items-center">
       <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={avatar} alt="" />
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
         {first_name} {last_name}
       </h5>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{username}</p>
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{email}</p>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">{username}</p>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">{email}</p>
     </div>
   );
 }
