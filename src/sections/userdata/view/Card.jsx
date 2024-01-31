cb3ee8dcafd0f432ea9e16e51d18349c32c7c3d1
hammadzahid52 <hamadzahid57@gmail.com>
2024-01-31 11:33:07 +0500

    Card UI Changes

--- src/sections/userdata/view/Card.jsx
+++ src/sections/userdata/view/Card.jsx
@@ -11,14 +11,14 @@ Card.propTypes = {
 
 function Card({ first_name, last_name, username, email, avatar }) {
   return (
-    <div class="max-w-sm p-7 bg-white border border-gray-200 rounded-lg shadow dark:bg-white flex flex-col items-center space-y-20">
-      <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={avatar} alt="" />
-      <div class="flex flex-col items-center space-y-3">
-        <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
+    <div className="max-w-sm p-7 bg-white border border-gray-200 rounded-lg shadow dark:bg-white flex flex-col items-center space-y-20">
+      <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={avatar} alt="" />
+      <div className="flex flex-col items-center space-y-3">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
           {first_name} {last_name}
         </h5>
-        <p class="font-normal text-gray-700 dark:text-gray-600">{username}</p>
-        <p class="font-normal text-gray-700 dark:text-gray-600">{email}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-600">{username}</p>
+        <p className="font-normal text-gray-700 dark:text-gray-600">{email}</p>
       </div>
     </div>
   );
